97b4d04e6110d543ba5240e9dedbcdd9afbd7e59
oui <ouibene@gmail.com>
2020-01-04 19:46:55 +0900

    0104 variable, constant data type

---
+++ MyPlayground0104.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+// command + slash (toggle comment)
+
+/* variable, constant
+ * lowerCamelCase
+ */
+var name = "oui"
+name
+name = "lye"
+name
+
+//type inference
+//literal
+var age = 15
+age
+age = 30
+age
+
+let year = 2020
+let birth: Int64 = 1991
+//cannot assign new value to assignment operator
+
+/* Data type */
+type(of: name)
+type(of: age)
+type(of: birth)
+
+var degree = 3.14
+type(of: degree)
+//option key + click variable name to see type of variables
+
+Int.max //8byte
+Int8.max
+Int16.max
+Int32.max
+Int64.max
+Int.min
+Int8.min
+Int16.min
+Int32.min
+Int64.min
+
+//Unsigned
+UInt.max
+UInt.min
+
+//Swift uses let more than var
