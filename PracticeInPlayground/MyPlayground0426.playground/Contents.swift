8301d7ed40d2805a00cfaa8e6dcc82d3d4512045
oui <ouibene@gmail.com>
2020-04-26 15:31:58 +0900

    Memory

---
+++ PracticeInPlayground/MyPlayground0426.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+/* memory */
+//중요: let 으로 변수가 선언되어있을 때 구조체(값형식)와 클래스(참조형식)의 차이
+
+/*
+ ARC (참조형식과 관련)
+ Automatic Reference Counting
+ 컴파일러가 특정 부분에 메모리 관리 코드를 넣어줌
+ <->
+ MRC
+ */
+
+/**/
