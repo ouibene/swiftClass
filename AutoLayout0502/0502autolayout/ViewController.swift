88048e9372418318223a332a8b3de642ce9fd60b
oui <ouibene@gmail.com>
2020-05-03 16:06:17 +0900

    AutoLayout

---
+++ AutoLayout0502/0502autolayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  0502autolayout
+//
+//  Created by oui on 5/2/20.
+//  Copyright © 2020 oui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
